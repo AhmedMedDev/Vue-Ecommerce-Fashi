ec134dd410a1543e5c22093aac4805d0c80e2858
dedoa82 <67242566+dedoa82@users.noreply.github.com>
2021-07-10 11:24:49 -0700

    Completing Home View

--- src/components/index-view/InstagramPhoto.vue
+++ src/components/index-view/InstagramPhoto.vue
@@ -1,36 +1,36 @@
 <template>
     <div class="instagram-photo">
-        <div class="insta-item set-bg" :data-setbg="require('../../../public/assets/img/insta-1.jpg')">
+        <div class="insta-item set-bg" :style="setBG1">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
                 <h5><a href="#">colorlib_Collection</a></h5>
             </div>
         </div>
-        <div class="insta-item set-bg" :data-setbg="require('../../../public/assets/img/insta-2.jpg')">
+        <div class="insta-item set-bg" :style="setBG2">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
                 <h5><a href="#">colorlib_Collection</a></h5>
             </div>
         </div>
-        <div class="insta-item set-bg" :data-setbg="require('../../../public/assets/img/insta-3.jpg')">
+        <div class="insta-item set-bg" :style="setBG3">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
                 <h5><a href="#">colorlib_Collection</a></h5>
             </div>
         </div>
-        <div class="insta-item set-bg" :data-setbg="require('../../../public/assets/img/insta-4.jpg')">
+        <div class="insta-item set-bg" :style="setBG4">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
                 <h5><a href="#">colorlib_Collection</a></h5>
             </div>
         </div>
-        <div class="insta-item set-bg" :data-setbg="require('../../../public/assets/img/insta-5.jpg')">
+        <div class="insta-item set-bg" :style="setBG5">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
                 <h5><a href="#">colorlib_Collection</a></h5>
             </div>
         </div>
-        <div class="insta-item set-bg" :data-setbg="require('../../../public/assets/img/insta-6.jpg')">
+        <div class="insta-item set-bg" :style="setBG6">
             <div class="inside-text">
                 <i class="ti-instagram"></i>
                 <h5><a href="#">colorlib_Collection</a></h5>
@@ -40,8 +40,25 @@
 </template>
 
 <script>
-export default {
 
+import insta1 from '../../../public/assets/img/insta-1.jpg';
+import insta2 from '../../../public/assets/img/insta-2.jpg';
+import insta3 from '../../../public/assets/img/insta-3.jpg';
+import insta4 from '../../../public/assets/img/insta-4.jpg';
+import insta5 from '../../../public/assets/img/insta-5.jpg';
+import insta6 from '../../../public/assets/img/insta-6.jpg';
+
+export default {
+    data () {
+        return  {
+            setBG1: {background:`url(${insta1})`},      
+            setBG2: {background:`url(${insta2})`},      
+            setBG3: {background:`url(${insta3})`},      
+            setBG4: {background:`url(${insta4})`},      
+            setBG5: {background:`url(${insta5})`},      
+            setBG6: {background:`url(${insta6})`},      
+        };
+    }
 }
 </script>
 
--- src/components/layout/PartnerLogo.vue
+++ src/components/layout/PartnerLogo.vue
@@ -1,31 +1,43 @@
 <template>
     <div class="partner-logo">
         <div class="container">
-            <div class="logo-carousel owl-carousel">
-                <div class="logo-item">
-                    <div class="tablecell-inner">
-                        <img :src="require('../../../public/assets/img/logo-carousel/logo-1.png')" alt="">
+            <div class="logo-carousel">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="logo-item">
+                            <div class="tablecell-inner">
+                                <img :src="require('../../../public/assets/img/logo-carousel/logo-1.png')" alt="">
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="logo-item">
-                    <div class="tablecell-inner">
-                        <img :src="require('../../../public/assets/img/logo-carousel/logo-2.png')" alt="">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="logo-item">
+                            <div class="tablecell-inner">
+                                <img :src="require('../../../public/assets/img/logo-carousel/logo-2.png')" alt="">
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="logo-item">
-                    <div class="tablecell-inner">
-                        <img :src="require('../../../public/assets/img/logo-carousel/logo-3.png')" alt="">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="logo-item">
+                            <div class="tablecell-inner">
+                                <img :src="require('../../../public/assets/img/logo-carousel/logo-3.png')" alt="">
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="logo-item">
-                    <div class="tablecell-inner">
-                        <img :src="require('../../../public/assets/img/logo-carousel/logo-4.png')" alt="">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="logo-item">
+                            <div class="tablecell-inner">
+                                <img :src="require('../../../public/assets/img/logo-carousel/logo-4.png')" alt="">
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="logo-item">
-                    <div class="tablecell-inner">
-                        <img :src="require('../../../public/assets/img/logo-carousel/logo-5.png')" alt="">
-                    </div>
+                    <!-- <div class="col-lg-3 col-sm-6">
+                        <div class="logo-item">
+                            <div class="tablecell-inner">
+                                <img :src="require('../../../public/assets/img/logo-carousel/logo-5.png')" alt="">
+                            </div>
+                        </div>
+                    </div> -->
                 </div>
             </div>
         </div>
@@ -33,6 +45,7 @@
 </template>
 
 <script>
+
 export default {
 
 }
